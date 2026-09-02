1f3d9c4539c94b259ec894c8ebc43dc8e7772839
EveKeepsLearning <eve.b.mendez@gmail.com>
2026-09-02 14:20:05 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BAUER_CONFIG = {
-  SUPABASE_URL: 'https://eufimdrimpkzowlupre.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_YOUR_FULL_KEY_HERE'
+  SUPABASE_URL: 'https://eufimdrdimpkzowlupre.supabase.co/rest/v1/',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_F-nEDaSaUQPwy2rTKy71LA_H4gxArBt'
 };
