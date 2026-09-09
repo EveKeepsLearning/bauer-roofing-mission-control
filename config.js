9174612ce8e782e38551ee39c6315b0657a5af7e
EveKeepsLearning <eve.b.mendez@gmail.com>
2026-09-09 12:12:35 -0400

    Bump BRO release for sortable Jobs table

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.BAUER_CONFIG = {
-  APP_VERSION: '20260909-7',
+  APP_VERSION: '20260909-8',
   APP_URL: 'https://evekeepslearning.github.io/bauer-roofing-mission-control/',
   SUPABASE_URL: 'https://eufimdrdimpkzowlupre.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_F-nEDaSaUQPwy2rTKy71LA_H4gxArBt',
@@ -7,7 +7,7 @@ window.BAUER_CONFIG = {
 };
 
 (function(){
-  const VERSION='20260909-7';
+  const VERSION='20260909-8';
   const NAV_ITEMS=[
     ['today','Today','index.html?view=today'],
     ['phone','Phone Message','index.html?view=phone'],
