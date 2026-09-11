1a6e21d0df64232beeb8c39232c709544845dc7c
EveKeepsLearning <eve.b.mendez@gmail.com>
2026-09-11 11:51:31 -0400

    Update BRO test service worker to build 52

--- bro-test/sw-test.js
+++ bro-test/sw-test.js
@@ -1,4 +1,4 @@
-const BUILD='20260911-51-test';
+const BUILD='20260911-52-test';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim());});
 self.addEventListener('fetch',event=>{
